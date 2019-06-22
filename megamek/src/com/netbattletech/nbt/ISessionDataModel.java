6711cb816fef6b6b49b3cf7aa2c74fd60a321f19
Xavier <xavier@netbattletech.com>
2019-06-22 12:49:47 -0700

    new LFG dialog layout with callsign-editing support, and integration with backend to edit same

--- megamek/src/com/netbattletech/nbt/ISessionDataModel.java
+++ megamek/src/com/netbattletech/nbt/ISessionDataModel.java
@@ -5,8 +5,10 @@ import java.util.List;
 public interface ISessionDataModel {
     List<ILobby> lobbies();
     IChat chat();
+    IPlayer player();
     void addLobby(ILobby lobby);
     void removeLobby(ILobby lobby);
     void postChat(IChatEntry chatEntry);
+    void updatePlayer(IPlayer player);
     void clearLobbies();
 }
